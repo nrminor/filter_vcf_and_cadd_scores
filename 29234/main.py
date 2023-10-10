d1c1128d6653df6ba8c3ebf90391012646aeb6bc
Nicholas Minor <nrminor@wisc.edu>
2023-10-10 15:03:07 -0500

    first commit

---
+++ 29234/main.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python3
+
+"""
+DOWNSAMPLE A MULTI-SAMPLE VCF AND ASSOCIATED CADD SCORES
+--------------------------------------------------------
+"""
+
+import os
+import sys
+import argparse
+import subprocess
+import glob
+import gzip
+from dataclasses import dataclass
+from result import Result, Ok, Err
+import polars as pl
+
+@dataclass
+class NewFile:
+    """
+    A data class for returning a new file path resulting from a successful
+    subprocess run.
+    """
+    name: str
+    path: str
+
+def parse_command_line_args() -> Result[argparse.Namespace, str]:
+    """
+    Parses command line arguments.
+
+    Returns:
+        Result[argparse.Namespace, str]: Returns Ok(argparse.Namespace) if args could 
+        be parsed, else returns Err(str).
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vcf", "-v",
+                        type=str,
+                        required=True,
+                        help="Multisample VCF to subset.")
+    parser.add_argument("--cadd_table", "-c",
+                        type=str,
+                        required=True,
+                        help="CADD Score spreadsheet in TSV format.")
+    parser.add_argument("--animal_file", "-a",
+                        type=str,
+                        required=True,
+                        help="Text file with one animal name per line.")
+    args = parser.parse_args()
+    if len(args.items()) < 3:
+        return Err("Command line arguments failed to parse.")
+    return Ok(args)
+
+def unify_sample_names(animals: pl.DataFrame, vcf_path: str) -> Result[NewFile, str]:
+    """
+        This function double checks the VCF at the provided path to make
+        sure the sample names in the VCF are the same as the sample names
+        in the metadata. If they aren't, it looks for the metadata sample
+        names as substrings of the VCF sample names. If all metadata 
+        sample names are in fact substrings of the VCF sample names, it
+        creates a tab-delimited text file to guide BCFTools, which it
+        uses in a subprocess to rename VCF sample names.
+
+    Args:
+        animals: A Polars DataFrame of the sample IDs.
+        vcf_path: a character string filepath leading to the input VCF.
+
+    Returns:
+        Result[NewFile, str]: A Result containing either an instance of the 
+        NewFile dataclass, which contains the  file path to the new VCF output
+        by VCFtools, or an informative error message.
+    """
+
+    if not vcf_path.endswith(".vcf.gz") or not vcf_path.endswith(".vcf"):
+        return Err("VCF is not in a recognizable format. Please double \
+                   check that your VCF file is either gzip-compressed and \
+                   ends with '.vcf.gz', or is uncompressed and ends with \
+                   '.vcf'.")
+
+    # create a list of sample names from the VCF
+    vcf_samples = []
+    if vcf_path.endswith(".gz"):
+        with gzip.open(vcf_path, 'rb') as vcf_file:
+            for line in vcf_file:
+                if line.startswith(b'#CHROM'):
+                    # Split the line by tabs, and extract the sample IDs
+                    # starting from the 9th column (0-indexed)
+                    vcf_samples = line.decode('utf-8').strip().split("\t")[9:]
+                    break
+    if vcf_path.endswith(".vcf"):
+        with open(vcf_path, 'r', encoding="utf-8") as vcf_file:
+            for line in vcf_file:
+                if line.startswith('#CHROM'):
+                    # Split the line by tabs, and extract the sample IDs
+                    # starting from the 9th column (0-indexed)
+                    vcf_samples = line.strip().split('\t')[9:]
+                    break
+    if len(vcf_samples) == 0:
+        return Err("Failed to define any sample names in the provided VCF.")
+
+    # create a list of the sample names from the metadata
+    meta_samples = animals.select(
+        "Sample ID"
+    ).drop_nulls().to_series().to_list()
+
+    # match the two lists, permitting no samples to be unmatched
+    matched_sample_ids = []
+    for meta_sample in meta_samples:
+        for vcf_sample in vcf_samples:
+            if meta_sample in vcf_sample:
+                matched_sample_ids.append((vcf_sample, meta_sample))
+                break
+    if len(matched_sample_ids) == 0:
+        return Err("Failed to match sample IDs between VCF and provided animal names")
+
+    # write a two column text file, where the first column is names
+    # in the VCF (old names), and the second column is names in the
+    # metadata (new names)
+    with open('new_sample_names.txt', 'w', encoding="utf-8") as out_handle:
+        for line in matched_sample_ids:
+            print(f"{line[0]}\t{line[1]}", file=out_handle)
+
+    # create the BCFTools command and run it in a subprocess
+    new_vcf = "renamed_samples.vcf.gz"
+    with open(new_vcf, "w", encoding="utf-8") as vcf_handle:
+        bcftools_process = subprocess.Popen(("bcftools", "reheader", "--samples",
+                           "new_sample_names.txt", vcf_path), 
+                           stdout=subprocess.PIPE)
+        gzip_process = subprocess.Popen(["gzip", "-c"], stdin=bcftools_process.stdout,
+                                        stdout=vcf_handle, stderr=subprocess.PIPE)
+
+        # Allow vcftools to receive a SIGPIPE if gzip exits
+        if bcftools_process.stdout:
+            bcftools_process.stdout.close()
+
+        # Wait for processes to complete and get their exit codes and stderr
+        _, bcftools_stderr = bcftools_process.communicate()
+        _, gzip_stderr = gzip_process.communicate()
+
+    # Get the exit code and give an informative error message if it isn't 0
+    bcftools_exit_code = bcftools_process.returncode
+    if bcftools_exit_code != 0:
+        return Err(f"VCF renaming encountered an error with exit code {bcftools_exit_code}:\n\
+                       {bcftools_stderr}")
+    gzip_exit_code = gzip_process.returncode
+    if gzip_exit_code != 0:
+        return Err(f"VCF compression encountered an error with exit code {gzip_exit_code}:\n\
+                       {gzip_stderr}")
+
+    # clear out temporary sample name file
+    os.remove("new_sample_names.txt")
+
+    new_file = NewFile(new_vcf, os.path.abspath(new_vcf))
+
+    return Ok(new_file)
+
+def filter_vcf_samples(vcf_path: str, animals: str) -> Result[NewFile, str]:
+    """
+        Run VCFTools to filter a VCF based on a text file of sample IDs to keep.
+
+    Args:
+        vcf_path (str): The path to the input VCF file.
+        animals (str): The path to the text file containing sample IDs to keep.
+
+    Returns:
+        Result[NewFile, str]: A Result containing either the new file path to
+        the filtered filtered VCF, or an informative error message.
+    """
+
+    # handle the possibility that the VCF is not named or formatted correctly
+    if not vcf_path.endswith(".vcf.gz") or not vcf_path.endswith(".vcf"):
+        return Err("VCF is not in a recognizable format. Please double \
+                   check that your VCF file is either gzip-compressed and \
+                   ends with '.vcf.gz', or is uncompressed and ends with \
+                   '.vcf'.")
+
+    # set the VCFtools input format flag based on the VCF file name is now
+    # guaranteed to be present thanks to the above error-handling
+    if vcf_path.endswith(".vcf.gz"):
+        input_format = "--gzvcf"
+    else:
+        input_format = "--vcf"
+
+    # construct the VCFTools subprocess command as a list
+    command = [
+        "vcftools",
+        input_format, vcf_path,
+        "--keep", animals,
+        "--recode", 
+        "--stdout"
+    ]
+
+    # run VCFtools
+    new_vcf = "filtered.vcf.gz"
+    with open(new_vcf, "wb", encoding="utf-8") as vcf_handle:
+        vcftools_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        gzip_process = subprocess.Popen(["gzip", "-c"], stdin=vcftools_process.stdout,
+                                        stdout=vcf_handle, stderr=subprocess.PIPE)
+
+        # Allow vcftools to receive a SIGPIPE if gzip exits
+        if vcftools_process.stdout:
+            vcftools_process.stdout.close()
+
+        # Wait for processes to complete and get their exit codes and stderr
+        _, vcftools_stderr = vcftools_process.communicate()
+        _, gzip_stderr = gzip_process.communicate()
+
+    # Get the exit code and give an informative error message if it isn't 0
+    vcftools_exit_code = vcftools_process.returncode
+    if vcftools_exit_code != 0:
+        return Err(f"VCF filtering encountered an error with exit code {vcftools_exit_code}:\n\
+                       {vcftools_stderr}")
+    gzip_exit_code = gzip_process.returncode
+    if gzip_exit_code != 0:
+        return Err(f"VCF filtering encountered an error with exit code {gzip_exit_code}:\n\
+                       {gzip_stderr}")
+
+    # remove VCFtools log files
+    for file in glob.glob("*.log"):
+        os.remove(file)
+
+    new_file = NewFile(new_vcf, os.path.abspath(new_vcf))
+    return Ok(new_file)
+
+def collect_filtered_positions(vcf_path: str) -> Result[pl.LazyFrame, str]:
+    """
+        Use BCFtools to define the positions that are still present in the 
+        filtered VCF. This positions will be cross-referenced downstream
+        with a table of CADD scores, making it possible to filter a CADD
+        table without requiring that table to be annotated with sample IDs.
+    
+    Args:
+        vcf_path: the path to the VCF to be assessed.
+    
+    Returns:
+        Result[pl.LazyFrame, str]: A result containing either a Polars
+        LazyFrame of the positions, or an informative error message.
+    """
+
+    if not vcf_path.endswith(".vcf.gz") or not vcf_path.endswith(".vcf"):
+        return Err("VCF is not in a recognizable format. Please double \
+                   check that your VCF file is either gzip-compressed and \
+                   ends with '.vcf.gz', or is uncompressed and ends with \
+                   '.vcf'.")
+
+    # define the command
+    command = ["bcftools", "query", "-f", "%POS\n", vcf_path]
+
+    # Run the command and collect the output
+    result = subprocess.run(command,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            text=True, check=True)
+
+    # Check if the command was unsuccessful
+    exitcode = result.returncode
+    if exitcode != 0:
+        return Err(f"BCFtools failed to find positions in the provided VCF and\
+                   terminated with exit code {exitcode}:\n{result.stderr}")
+
+    # The stdout contains the positions, one per line
+    positions = result.stdout.strip().split("\n")
+
+    # Convert to integers and double check that the list isn't empty
+    positions = list(map(int, positions))
+    if len(positions) == 0:
+        return Err("BCFtools encountered a silent error such that the constructed\
+                   list of positions is empty")
+
+    # convert the list to a polars dataframe and return it
+    position_df = pl.LazyFrame({"Positions": positions})
+    return Ok(position_df)
+
+def main() -> None:
+    """
+    Main handles I/O, coordinates the flow of data through the above functions,
+    and exposes any errors.
+    """
+
+    # collect command line argument result
+    arg_result = parse_command_line_args()
+    match arg_result:
+        case Ok(result):
+            vcf_path = result.vcf
+            table_path = result.cadd_table
+            animals = result.animal_file
+        case Err(result):
+            sys.exit(f"{result}")
+
+    # make sure animal names match the sample IDs in the VCF
+    animals_df = pl.read_excel(animals,
+                            read_csv_options={"null_values": ["NA", ""]}).select(
+                                pl.col(["Sample ID"])).drop_nulls()
+    unify_result = unify_sample_names(animals_df, vcf_path)
+    match unify_result:
+        case Ok(result):
+            renamed_vcf_path = result.path
+        case Err(result):
+            sys.exit(f"{result}")
+
+    # filter the VCF
+    filtering_result = filter_vcf_samples(renamed_vcf_path, animals)
+    match filtering_result:
+        case Ok(result):
+            filtered_vcf = result.path
+        case Err(result):
+            sys.exit(f"{result}")
+
+    # define the positions
+    position_result = collect_filtered_positions(filtered_vcf)
+    match position_result:
+        case Ok(result):
+            positions = result
+        case Err(result):
+            sys.exit(f"{result}")
+
+    # lazily read the CADD Score table and filter its rows to only those
+    # that contain a mutation that is present in the newly filtered VCF
+    cadd_score_df = pl.scan_csv(table_path, separator = '\t')
+    final_cadd_scores = positions.join(
+        cadd_score_df, left_on="Positions", right_on="POSITION", how="left"
+        )
+    final_cadd_scores.sink_csv("filtered_cadd_scores.tsv", separator='\n')
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,2 @@
+# Filter VCF and CADD Scores together
+Filtering VCFs can be tedious. The Python script in this repo wraps `bcftools` and `vcftools` to make filtering do a desired subset of samples a little easier. It also brings large tables of [CADD Scores](https://cadd.gs.washington.edu/) with it, using [Polars LazyFrames](https://pola-rs.github.io/polars/py-polars/html/reference/lazyframe/index.html) to do so. Because it coordinates many moving parts, the script also uses elegant, Rust-like error handling thanks to the Python [`result`](https://pypi.org/project/result/) module.
